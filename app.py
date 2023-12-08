4b781f1e523c71de2cf75d03f891924f5f9a2fa2
kodai <kodaiadvance@icloud.com>
2023-12-08 13:22:57 +0900

    login-logout

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_migrate import Migrate
+from flask_login import LoginManager, login_user, logout_user, current_user, UserMixin, login_required
+from werkzeug.security import generate_password_hash
+import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # 秘密キーの設定
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.urandom(24)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    
-    def __repr__(self):
-        return f'<User {self.name}>'
-    
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -29,7 +38,8 @@ def register():
         email = request.form['email']
         password = request.form['password']
 
-        new_user = User(name=name, email=email, password=password)
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(name=name, email=email, password=hashed_password)
         try:
             db.session.add(new_user)
             db.session.commit()
@@ -39,17 +49,38 @@ def register():
             return "このメールアドレスは既に登録されています。"
     return render_template('register.html')
 
-@app.route('/registration_complete')
-def registration_complete():
-    return render_template('registration_complete.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+
+        user = User.query.filter_by(name=name, password=password).first()
+
+        if user:
+            login_user(user)
+            return redirect(url_for('stamp'))
+        else:
+            return "Failed Login"
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/stamp')
+@login_required
 def stamp():
     return render_template('stamp_rally.html')
 
-@app.route('/kenrokuen', methods=['GET', 'POST'])
+@app.route('/kenrokuen')
 def kenrokuen():
-    return render_template('kenrokuen.html')
+    render_template('kenrokuen.html')
+
+# その他のルートと関数...
 
 if __name__ == '__main__':
     with app.app_context():
