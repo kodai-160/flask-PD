968e3bbbaa9fc5940238bd544ed0321ed6786097
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2023-12-09 19:33:47 +0900

    コメント関連の変更

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
