ebbdca121453ac78d469ad061e794794cca3cd48
kodai <kodaiadvance@icloud.com>
2023-12-15 14:05:34 +0900

    morning

--- app.py
+++ app.py
@@ -116,12 +116,52 @@ def stamp():
     # スタンプの状態を辞書として抽出
     stamps_status = {stamp.name: stamp.collected for stamp in user_stamps}
     # テンプレートにスタンプの状態を渡す
-    return render_template('stamp_rally.html', stamps_status=stamps_status)
+    return render_template('stamp_rally_morning.html', stamps_status=stamps_status)
 
-@app.route('/kenrokuen')
-def kenrokuen():
+@app.route('/morning/stamp1')
+def stamp1():
     comments = Comment.query.all()
-    return render_template('kenrokuen.html', comments=comments)
+    return render_template('/morning/stamp1.html', comments=comments)
+
+@app.route('/morning/stamp2')
+def stamp2():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp2.html', comments=comments)
+
+@app.route('/morning/stamp3')
+def stamp3():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp3.html', comments=comments)
+
+@app.route('/morning/stamp4')
+def stamp4():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp4.html', comments=comments)
+
+@app.route('/morning/stamp5')
+def stamp5():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp5.html', comments=comments)
+
+@app.route('/morning/stamp6')
+def stamp6():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp6.html', comments=comments)
+
+@app.route('/morning/stamp7')
+def stamp7():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp7.html', comments=comments)
+
+@app.route('/morning/stamp8')
+def stamp8():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp8.html', comments=comments)
+
+@app.route('/morning/stamp9')
+def stamp9():
+    comments = Comment.query.all()
+    return render_template('/morning/stamp9.html', comments=comments)
 
 @app.route('/comment', methods=['GET', 'POST'])
 def comment():
